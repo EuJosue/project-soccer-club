45f4927b786c6d46cdde80b1b73e896bc9c0840d
Josué Kirchner <josueantoniok@gmail.com>
2023-05-10 20:20:37 -0300

    feat: Sequelize Migration create matches table created

---
+++ app/backend/src/database/migrations/3-create-matches-table.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      home_team_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  },
+};
