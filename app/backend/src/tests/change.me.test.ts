38f173120712f07162e52a90957491f6f7bbd133
Josué Kirchner <josueantoniok@gmail.com>
2023-05-10 20:06:28 -0300

    feat: more test for Endpoint /teams/:id implemented

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -32,13 +32,30 @@ describe('Testes de Integração', () => {
     });
 
     describe('GET /teams/:id', async () => {
-      it('Se responde com status 200 e um time', async () => {
+      it('Se passado um id válido responde com status 200 e um time', async () => {
+        sinon
+          .stub(Team, "findByPk")
+          .resolves(teams[0] as unknown as Team);
+
         const { body, status } = await chai
           .request(app).get('/teams/1');
 
         expect(status).to.be.equal(200);
         expect(body).to.be.an('object');
         expect(body).to.be.deep.equal(teams[0]);
+      });
+
+      it('Se passado um id inválido, response com status 404 e Team not found', async () => {
+        sinon
+          .stub(Team, "findByPk")
+          .resolves(null);
+
+        const { body, status } = await chai
+          .request(app).get('/teams/3');
+          
+        expect(status).to.be.equal(404);
+        expect(body).to.be.an('object');
+        expect(body).to.be.deep.equal({ message: 'Team not found' });
       })
     });
   });
