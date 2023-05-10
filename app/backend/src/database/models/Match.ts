10886c71034fa8988b41dd6e2142e30e1c3d22ea
Josué Kirchner <josueantoniok@gmail.com>
2023-05-10 20:18:16 -0300

    feat: Match Sequelize Model created

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+});
+
+export default Match;
