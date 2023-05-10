f07f34b7e66fce7c8ff598c857eaabab6d71c007
Josué Kirchner <josueantoniok@gmail.com>
2023-05-10 19:09:29 -0300

    feat: method findByPk created in TeamService

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,7 +1,18 @@
+import { Identifier } from 'sequelize';
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../utils/ApiError';
 import TeamModel from '../models/TeamModel';
 
 export default class TeamService {
   constructor(private teamModel = new TeamModel()) {}
 
   async findAll() { return this.teamModel.findAll(); }
+
+  async findByPk(pk: Identifier) {
+    const team = await this.teamModel.findByPk(pk);
+
+    if (!team) throw new ApiError(StatusCodes.NOT_FOUND, 'Team not found');
+
+    return team;
+  }
 }
