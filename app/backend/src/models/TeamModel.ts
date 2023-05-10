da623cf9f7078047ace140e45ce347b7a3238117
Josué Kirchner <josueantoniok@gmail.com>
2023-05-10 18:57:06 -0300

    feat: method findByPk created in TeamModel

--- app/backend/src/models/TeamModel.ts
+++ app/backend/src/models/TeamModel.ts
@@ -1,7 +1,10 @@
+import { Identifier } from 'sequelize';
 import Team from '../database/models/Team';
 
 export default class TeamModel {
   constructor(private _db = Team) {}
 
   async findAll() { return this._db.findAll(); }
+
+  async findByPk(pk: Identifier) { return this._db.findByPk(pk); }
 }
