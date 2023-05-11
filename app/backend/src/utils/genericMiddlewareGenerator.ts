021d38e93cdcdc3d71e716a6bcd12bbbadddaa2b
Josué Kirchner <josueantoniok@gmail.com>
2023-05-11 16:18:22 -0300

    feat: Generic Middleware Generator add second optional param

--- app/backend/src/utils/genericMiddlewareGenerator.ts
+++ app/backend/src/utils/genericMiddlewareGenerator.ts
@@ -1,19 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import { Schema } from 'joi';
+import { Schema, ValidationError } from 'joi';
 import statusCode from './statusCode';
 
-export default (joi: Schema) => (
+const errorHandlerFunction = (
+  error: ValidationError,
+  _req: Request,
+  res: Response,
+) => {
+  const { type } = error.details[0];
+  console.log(type);
+
+  return res.status(statusCode(type)).json({ message: error.message });
+};
+
+export default (joi: Schema, errorHandler = errorHandlerFunction) => (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   const { error } = joi.validate(req.body);
 
-  if (error) {
-    const { type } = error.details[0];
-
-    return res.status(statusCode(type)).json({ message: error.message });
-  }
+  if (error) return errorHandler(error, req, res);
 
   return next();
 };
