7a6f8fd6e3cdc41437d0478ab364c49b2e28f728
Josué Kirchner <josueantoniok@gmail.com>
2023-05-11 17:11:30 -0300

    feat: added more 2 tests for Login.test.ts POST /login; feat: added tests for GET /login/role;

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -7,6 +7,7 @@ import { app } from '../app';
 
 import { users } from './mocks/Login.mock';
 import User from '../database/models/User';
+import Auth from '../utils/Auth';
 
 chai.use(chaiHttp);
 
@@ -52,7 +53,7 @@ describe('login', () => {
           email: 'adminwrongemail@admin.com',
           password: 'secret_admin'
         });
-          
+
       expect(status).to.be.equal(401);
       expect(body).to.be.an('object');
       expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
@@ -69,6 +70,42 @@ describe('login', () => {
         .send({
           email: 'admin@admin.com',
           password: 'wrong_secret_admin'
+        });
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
+    });
+
+    it('Se passado um email com formato inválido responde com status 401 e Invalid email or password', async () => {
+      sinon
+        .stub(User, 'findOne')
+        .resolves(users[0] as unknown as User);
+
+      const { body, status } = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'adminwrongemail',
+          password: 'wrong_secret_admin'
+        });
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Invalid email or password' });
+    });
+
+    it('Se passado uma senha de tamanho inválido responde com status 401 e Invalid email or password', async () => {
+      sinon
+        .stub(User, 'findOne')
+        .resolves(users[0] as unknown as User);
+
+      const { body, status } = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+          password: 'wrong'
         });
 
       expect(status).to.be.equal(401);
@@ -95,6 +132,41 @@ describe('login', () => {
     });
   });
 
+  describe('GET /login/role', () => {
+    it('Se não for passado um token responde com status 401 e Token not found', async () => {
+      const { body, status } = await chai
+        .request(app)
+        .get('/login/role');
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Token not found' });
+    });
+
+    it('Se passado um token inválido responde com status 401 e Token must be a valid token', async () => {
+      const { body, status } = await chai
+        .request(app)
+        .get('/login/role')
+        .auth('wrong_token', { type: 'bearer' });
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Token must be a valid token' });
+    });
+
+    it('Se passado um token válido responde com status 200 e o role do usuário', async () => {
+      const token = new Auth().generateToken({ id: 1, email: 'admin@admin.com' });
+
+      const { body, status } = await chai
+        .request(app)
+        .get('/login/role')
+        .auth(token, { type: 'bearer' });
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ role: 'admin' });
+    });
+  });
+
   afterEach(sinon.restore);
 });
-
