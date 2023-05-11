6946a4d25add19362e6be7f0f6de2c4d00c18e39
Josué Kirchner <josueantoniok@gmail.com>
2023-05-11 17:25:49 -0300

    feat: method findById implemented in UserService

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,5 +1,17 @@
+import { StatusCodes } from 'http-status-codes';
+import User from '../database/models/User';
 import UserModel from '../models/UserModel';
+import IId from '../interfaces/IId';
+import ApiError from '../utils/ApiError';
 
 export default class UserService {
-  constructor(private userModel = new UserModel()) {}
+  constructor(private _userModel = new UserModel()) {}
+
+  async findById({ id }: IId) {
+    const user = await this._userModel.findById(id) as User;
+
+    if (!user) throw new ApiError(StatusCodes.NOT_FOUND, 'User not found');
+
+    return user;
+  }
 }
