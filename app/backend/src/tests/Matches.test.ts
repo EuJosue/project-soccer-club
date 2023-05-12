01eff0320bb486cfe04bd4df8069f1797bc2c57e
Josué Kirchner <josueantoniok@gmail.com>
2023-05-12 18:56:21 -0300

    feat: tests for ENDPOINT PATCH /matches/1 implemented

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -91,7 +91,7 @@ describe('Matches', () => {
       expect(body).to.be.deep.equal({ message: 'Finished' });
     });
 
-    it.only('Se passado um id inválido responde com status 404 e Match not found', async () => {
+    it('Se passado um id inválido responde com status 404 e Match not found', async () => {
       sinon
         .stub(Match, 'update')
         .resolves([0]);
@@ -103,6 +103,80 @@ describe('Matches', () => {
       expect(status).to.be.equal(404);
       expect(body).to.be.an('object');
       expect(body).to.be.deep.equal({ message: 'Match not found' });
+    });
+  });
+
+  describe('PATCH /matches/:id', () => {
+    it('Se não for passado um token responde com status 401 e Token not found', async () => {
+      const { body, status } = await chai
+        .request(app).patch('/matches/1');
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Token not found' });
+    });
+
+    it('Se passado um token inválido responde com status 401 e Token must be a valid token', async () => {
+      const { body, status } = await chai
+        .request(app).patch('/matches/1')
+        .auth('invalid_token', { type: 'bearer' });
+
+      expect(status).to.be.equal(401);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Token must be a valid token' });
+    });
+
+    it('Se passado um id válido responde com status 200 e Updated', async () => {
+      sinon
+        .stub(Match, 'update')
+        .resolves([1]);
+
+      const { body, status } = await chai
+        .request(app).patch('/matches/1')
+        .auth(token, { type: 'bearer' })
+        .send({
+          homeTeamGoals: 3,
+          awayTeamGoals: 1
+        });
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Updated' });
+    });
+
+    it('Se passado um id inválido responde com status 404 e Match not found', async () => {
+      sinon
+        .stub(Match, 'update')
+        .resolves([0]);
+
+      const { body, status } = await chai
+        .request(app).patch('/matches/3')
+        .auth(token, { type: 'bearer' })
+        .send({
+          homeTeamGoals: 3,
+          awayTeamGoals: 1
+        });
+
+      expect(status).to.be.equal(404);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Match not found' });
+    });
+
+    it('Se passado um id válido e um body inválido responde com status 400 e Invalid body', async () => {
+      sinon
+        .stub(Match, 'update')
+        .resolves([1]);
+
+      const { body, status } = await chai
+        .request(app).patch('/matches/1')
+        .auth(token, { type: 'bearer' })
+        .send({
+          homeTeamGoals: 3
+        });
+
+      expect(status).to.be.equal(400);
+      expect(body).to.be.an('object');
+      expect(body).to.be.deep.equal({ message: 'Bad request' });
     });
   });
 
