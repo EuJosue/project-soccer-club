7e89d1cc3458a1d60cdd160b40523ec072be7f92
Josué Kirchner <josueantoniok@gmail.com>
2023-05-12 16:17:02 -0300

    feat: more tests for Matches.test.ts

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -4,7 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { matches } from './mocks/Matches.mock';
+import { matches, matchesInProgress, matchesNotInProgress } from './mocks/Matches.mock';
+import Match from '../database/models/Match';
 
 chai.use(chaiHttp);
 
@@ -12,13 +13,43 @@ const { expect } = chai;
 
 describe('Matches', () => {
   describe('GET /matches', () => {
-    it('Se responde com status 200 e um array de partidas', async () => {
+    it('Se quando chamado sem inProgress responde com status 200 e um array com todas as partidas', async () => {
+      sinon
+        .stub(Match, 'findAll')
+        .resolves(matches as unknown as Match[]);
+
       const { body, status } = await chai
         .request(app).get('/matches');
 
       expect(status).to.be.equal(200);
       expect(body).to.be.an('array');
       expect(body).to.be.deep.equal(matches);
+    });
+
+    it('Se quando chamado com inProgress true responde com status 200 e um array com as partidas em progresso', async () => {
+      sinon
+        .stub(Match, 'findAll')
+        .resolves(matchesInProgress as unknown as Match[]);
+
+      const { body, status } = await chai
+        .request(app).get('/matches?inProgress=true');
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.an('array');
+      expect(body).to.be.deep.equal(matchesInProgress);
+    });
+
+    it('Se quando chamado com inProgress false responde com status 200 e um array com as partidas em progresso', async () => {
+      sinon
+        .stub(Match, 'findAll')
+        .resolves(matchesNotInProgress as unknown as Match[]);
+
+      const { body, status } = await chai
+        .request(app).get('/matches?inProgress=false');
+
+      expect(status).to.be.equal(200);
+      expect(body).to.be.an('array');
+      expect(body).to.be.deep.equal(matchesNotInProgress);
     });
   });
 
