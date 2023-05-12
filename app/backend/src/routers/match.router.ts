c282bd7db110ec000461f4d4eadcfa0f89d60699
Josué Kirchner <josueantoniok@gmail.com>
2023-05-12 15:50:14 -0300

    feat: ENDPOINT PATCH /matches/:id/finish implemented

--- app/backend/src/routers/match.router.ts
+++ app/backend/src/routers/match.router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import validateToken from '../middlewares/validateToken';
 import MatchController from '../controllers/MatchController';
 
 const router = Router();
 const matchController = new MatchController();
 
 router.get('/', (req, res) => matchController.findAllWithTeamName(req, res));
+
+router.patch('/:id/finish', validateToken, (req, res) => matchController.finishMatch(req, res));
 
 export default router;
