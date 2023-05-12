ef206038ae54e310b03ecca842998eda7cba8eab
Josué Kirchner <josueantoniok@gmail.com>
2023-05-12 14:19:45 -0300

    feat: class MatchModel created with methods findAll and findAllWithTeamName implemented

---
+++ app/backend/src/models/MatchModel.ts
@@ -0,0 +1,9 @@
+import Match from '../database/models/Match';
+
+export default class MatchModel {
+  constructor(private _db = Match) {}
+
+  async findAll() { return this._db.findAll(); }
+
+  async findAllWithTeamName() { return this._db.scope('withTeamName').findAll(); }
+}
