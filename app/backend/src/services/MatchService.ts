198391164f9af32992a2fb2f3de9d8812ab0406e
Josué Kirchner <josueantoniok@gmail.com>
2023-05-12 18:26:26 -0300

    feat: method update implemented in MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,3 +1,6 @@
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../utils/ApiError';
+import MatchUpdate from '../interfaces/MatchUpdate';
 import MatchModel from '../models/MatchModel';
 
 export default class MatchService {
@@ -12,4 +15,10 @@ export default class MatchService {
   }
 
   async finishMatch(id: number) { return this._matchModel.finishMatch(id); }
+
+  async update(id: number, changes: MatchUpdate) {
+    const [affectedRows] = await this._matchModel.update(id, changes);
+
+    if (!affectedRows) throw new ApiError(StatusCodes.NOT_FOUND, 'Match not found');
+  }
 }
