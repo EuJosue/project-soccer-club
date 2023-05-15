54cccb3036ecadf31625a7bc21d74f4a774caab2
Josué Kirchner <josueantoniok@gmail.com>
2023-05-15 17:16:32 -0300

    feat: implemented ENDPOINTS GET:   - /leaderboard/away   - /leaderboard

--- app/backend/src/routers/leaderboard.router.ts
+++ app/backend/src/routers/leaderboard.router.ts
@@ -4,6 +4,8 @@ import LeaderboardController from '../controllers/LeaderboardController';
 const router = Router();
 const leaderboardController = new LeaderboardController();
 
+router.get('/', (req, res) => leaderboardController.findAll(req, res));
+router.get('/away', (req, res) => leaderboardController.findAllAway(req, res));
 router.get('/home', (req, res) => leaderboardController.findAllHome(req, res));
 
 export default router;
