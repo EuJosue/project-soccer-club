dd1a544d43f02009d7f6d93993ef19e62a6e25fe
Josué Kirchner <josueantoniok@gmail.com>
2023-05-15 17:13:30 -0300

    feat: implemented 2 methods at LeaderboardController:   - findAllAway   - countAwayGames

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -9,4 +9,16 @@ export default class LeaderboardController {
 
     return res.json(leaderboard);
   }
+
+  async findAllAway(_req: Request, res: Response) {
+    const leaderboard = await this._Service.findAllAway();
+
+    return res.json(leaderboard);
+  }
+
+  async findAll(_req: Request, res: Response) {
+    const leaderboard = await this._Service.findAll();
+
+    return res.json(leaderboard);
+  }
 }
